f06f1a536730a8d77cc3498c2cced92827c2cf9a
Nonso Nwabuokei <nnwabuokei@gmail.com>
2024-10-15 20:24:19 +0200

    - Init project with its dependencies - Add PrimeNG UI dependency - Migrate project components to standalone.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {PrimeNGConfig} from "primeng/api";
+import {Aura} from "primeng/themes/aura";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true
 })
 export class AppComponent {
-  title = 'Userlane Angular Task';
+
+  constructor(private config: PrimeNGConfig) {
+    this.config.theme.set({ preset: Aura })
+  }
 }
